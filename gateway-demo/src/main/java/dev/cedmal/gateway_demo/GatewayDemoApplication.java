1ecc3726a0e749dec1e8dec832104214a915494b
Cedric <cedriccoopers@gmail.com>
2024-11-12 17:37:17 +0200

    initial commit

---
+++ gateway-demo/src/main/java/dev/cedmal/gateway_demo/GatewayDemoApplication.java
@@ -0,0 +1,13 @@
+package dev.cedmal.gateway_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayDemoApplication.class, args);
+	}
+
+}
---
+++ gateway-demo/src/test/java/dev/cedmal/gateway_demo/GatewayDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cedmal.gateway_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
